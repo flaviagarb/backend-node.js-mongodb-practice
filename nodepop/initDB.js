f77d69fa0b6aff57d42c24840aa9a45423ff5895
Flavia Garbetta <flavia.garbetta@gmail.com>
2025-03-27 11:52:00 +0100

    Add boostrap cards

--- nodepop/initDB.js
+++ nodepop/initDB.js
@@ -1,9 +1,10 @@
 import readline from 'node:readline/promises'
 import connectMongoose from "./lib/connectMongoose.js";
 import Product from './models/Product.js';
-import User from './models/User.js'
+import User from './models/User.js';
+import Chance from 'chance';
 
-
+const chance = new Chance();
 const randomImg = Math.floor(Math.random() * 1000)
 const connection = await connectMongoose()
 console.log('Connected to MongoDB:', connection.name)
@@ -31,24 +32,38 @@ async function initDBNodepop() {
     // create product
     const insertProduct = await Product.insertMany([
         {
-            name: 'Mobile',
+            name: 'Samsung Galaxy A15',
             price: 500,
-            image: `https://picsum.photos/id/${Math.floor(Math.random() * 1000)}/300/200`,
+            image: 'https://m.media-amazon.com/images/I/7159IY51UIL._AC_UF1000,1000_QL80_.jpg',
             tags: ['mobile'],
             owner: user._id
         },
         {
-            name: 'Tablet',
-            price: 600,
-            image: `https://picsum.photos/id/${Math.floor(Math.random() * 1000)}/300/200`,
+            name: 'Tablet Galaxy S6 Lite',
+            price: 100,
+            image: 'https://cdn.grupoelcorteingles.es/SGFM/dctm/MEDIA03/202404/05/00115217117389____8__1200x1200.jpg',
             tags: ['mobile', 'lifestyle'],
             owner: user._id
         },
         {
-            name: 'MTV',
-            price: 700,
-            image: `https://picsum.photos/id/${Math.floor(Math.random() * 1000)}/300/200`,
+            name: 'TV LG 32LQ',
+            price: 200,
+            image: 'https://www.lg.com/content/dam/channel/wcms/es/images/television/55ut73006la_aeuq_eees_es_c/gallery/large1.jpg',
             tags: ['lifestyle'],
+            owner: admin._id
+        },
+        {
+            name: 'Gameboy',
+            price: 700,
+            image: 'https://i5.walmartimages.com/seo/Nintendo-GameBoy-Game-Boy-Color-Atomic-Purple-Authentic-100-OEM_af704870-eee2-401a-b2d1-a5b463213a37.3edcbe62993c043ee53a9cc925ef5c40.png',
+            tags: ['lifestyle', 'mobile'],
+            owner: admin._id
+        },
+        {
+            name: 'Iphone 13',
+            price: 800,
+            image: 'https://colourmobiletomelloso.es/wp-content/uploads/2021/09/Iphone-13-azul-Colour-Mobile-Tomelloso.jpg',
+            tags: ['mobile'],
             owner: admin._id
         },
     ])
